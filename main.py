68c5613ccbc307308722420aedfb6d964c7fdd2e
Joshua Jung <tojjung01@gmail.com>
2019-11-10 10:04:35 -0600

    Following the tutorial and using real git techniques

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ import discord
 from boto.s3.connection import S3Connection
 
 token = S3Connection(os.environ['DISCORD_TOKEN'])
-
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-async def onMessage(message):
-    if message.author == client.user:
-        return
-    if message.content == "ping":
-        message.channel.send("pong")
+@client.event
+async def on_message(message):
+    if message.content.find("!ping") != -1:
+        await message.channel.send("pong")
 
 client.run(token)
