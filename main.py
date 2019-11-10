06ead76cc1e2df79a828343daeb0f8f3ea092924
Joshua Jung <tojjung01@gmail.com>
2019-11-10 14:50:43 -0600

    commit

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 #This Discord bot will have people split into balanced teams
 import os
 import discord
-from boto.s3.connection import S3Connection
 
-token = S3Connection(os.environ["DISCORD_TOKEN"])
-print(token)
+token = os.environ["DISCORD_TOKEN"]
 client = discord.Client()
 
 @client.event
